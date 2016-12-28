3376682483546c3a79abac0da8233fc129c6c5c8
ybs007 <Yair0506474781@gmail.com>
2016-12-28 16:43:04 +0200

    ee

--- ChessProject/Board.cpp
+++ ChessProject/Board.cpp
@@ -1,14 +1,24 @@
 #include "Board.h"
 
+/**
+This functions is creating a board object
+input:
+	none
+*/
 Board::Board()
 {
-	createBoard();
+	createBoard(); //calling to create board
 	_isWhiteTurn = true;
 	_cache1 = nullptr;
 	_cache2 = nullptr;
 
 }
 
+/**
+This function is deleting the memory allocated 
+for board
+input: none
+*/
 Board::~Board()
 {
 	for (int i = 0; i < BOARD_SIZE; i++)
@@ -17,25 +27,32 @@ Board::~Board()
 		{
 			if (_board[i][j] != nullptr)
 			{
-				delete _board[i][j];
+				delete _board[i][j]; //deleting the memory allocated
 			}
 		}
 
-		delete[] _board[i];
+		delete[] _board[i]; //deleting the memory allocated
 	}
 
-	delete[] _board;
+	delete[] _board; //deleting the memory allocated
 }
 
+/**
+This function is creating a board for the start of the program
+input:
+	none
+output:
+	none
+*/
 void Board::createBoard()
 {
 	*_board = new ChessPiece*[BOARD_SIZE];
 	for (int i = 0; i < BOARD_SIZE; i++)
 	{
-		_board[i] = new ChessPiece*[BOARD_SIZE];
+		_board[i] = new ChessPiece*[BOARD_SIZE]; //intalizing the array
 	}
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 2; i++)//adding black side
 	{
 		for (int j = 0; j < BOARD_SIZE; j++)
 		{
@@ -43,7 +60,7 @@ void Board::createBoard()
 		}
 	}
 
-	for (int i = 2; i < BOARD_SIZE - 2; i++)
+	for (int i = 2; i < BOARD_SIZE - 2; i++)//adding nulls for empty spaces
 	{
 		for (int j = 0; j < BOARD_SIZE; j++)
 		{
@@ -51,7 +68,7 @@ void Board::createBoard()
 		}
 	}
 
-	for (int i = BOARD_SIZE - 2; i < BOARD_SIZE; i++)
+	for (int i = BOARD_SIZE - 2; i < BOARD_SIZE; i++) //adding white side
 	{
 		for (int j = 0; j < BOARD_SIZE; j++)
 		{
